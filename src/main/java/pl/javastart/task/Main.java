8b4cf33c7fbb1649aac84be4ebcd36c9850bbf50
Pawel Maliszewski <pablo999455@gmail.com>
2023-02-08 09:43:20 +0100

    Solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,29 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        double sum = 0;
+
+        System.out.println("Podaj rozmiar tablicy:");
+        int arraySize = sc.nextInt();
+        sc.nextLine();
+
+        double[] arrays = new double[arraySize];
+
+        System.out.println("Wprowadź " + arraySize + " liczb:");
+        for (int i = 0; i < arrays.length; i++) {
+            arrays[i] = sc.nextDouble();
+        }
+
+        for (double array : arrays) {
+            sum += array * array;
+        }
+        System.out.println("Suma kwadratów wprowadzonych liczb to " + sum);
+        sc.close();
     }
 }
